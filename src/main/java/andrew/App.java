b85b59c5c4616ae520be0b088f3dc61d43c577c3
Andrew Fang <welcomeandrew@gmail.com>
2022-04-09 22:41:13 -0400

    added two methods

--- src/main/java/andrew/App.java
+++ src/main/java/andrew/App.java
@@ -9,9 +9,15 @@ public class App {
         int a = 10;
         int b = 20;
         int c = 22;
+        int d = 1;
 
         int total = sum(a, b, c);
         System.out.println("sum of 3 numbers: " + total);
+        int totalp = product(a, b, c, d);
+        System.out.println("Product of 4 numbers: " + totalp);
+        int totals = difference(a, b);
+        System.out.println("Difference of 2 numbers: " + totals);
+
     }
 
     public static String greet(String name) {
@@ -21,14 +27,28 @@ public class App {
     }
 
     public static int sum(int a, int b, int c) {
-       int sum = a + b + c;
-       return sum;
+        int sum = a + b + c;
+        return sum;
+    }
 
+    public static int product(int a, int b, int c, int d) {
+        int product = a * b * c * d;
+        return product;
     }
 
 
-
-
-
+    public static int difference(int a, int b) {
+        int difference = a - b;
+        return difference;
+    }
 
 }
+
+
+
+
+
+
+
+
+
